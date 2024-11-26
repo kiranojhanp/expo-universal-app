660033e3a00366225e5cbaaaa53d84973a935d63
Kiran <kiranojha1226@gmail.com>
2024-11-26 20:43:45 +0800

    chore: use postgres.js instead of pg

--- trpc-server/db/index.ts
+++ trpc-server/db/index.ts
@@ -1,13 +1,10 @@
 import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 import * as schema from "./schema";
 
-export const db = drizzle({
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-    idle_timeout: 20,
-  },
-  schema,
+export const connection = postgres(process.env.DATABASE_URL!, {
+  idle_timeout: 20,
 });
 
+export const db = drizzle(connection, { schema });
 export type DB = typeof db;
