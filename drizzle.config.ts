a3171e120fa0edfcee9e0a2304cd60c546c04910
Kiran <kiranojha1226@gmail.com>
2024-11-27 09:21:44 +0800

    feat: fix drizzle push

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,11 +2,35 @@ import { defineConfig } from "drizzle-kit";
 import { DATABASE_PREFIX } from "@/constants/DB";
 
 export default defineConfig({
-  schema: "./trpc-server/db/schema.ts",
-  out: "./drizzle",
+  out: "./drizzle/migrations",
   dialect: "postgresql",
+  schema: "./trpc-server/db/schema.ts",
+
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
+
+  schemaFilter: "public",
   tablesFilter: [`${DATABASE_PREFIX}_*`],
+
+  introspect: {
+    casing: "camel",
+  },
+  migrations: {
+    prefix: "timestamp",
+    table: "__drizzle_migrations__",
+    schema: "public",
+  },
+
+  entities: {
+    roles: {
+      provider: "",
+      exclude: [],
+      include: [],
+    },
+  },
+
+  breakpoints: true,
+  strict: true,
+  verbose: true,
 });
--- trpc-server/db/index.ts
+++ trpc-server/db/index.ts
@@ -2,9 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-export const connection = postgres(process.env.DATABASE_URL!, {
-  idle_timeout: 20,
-});
+export const queryClient = postgres(process.env.DATABASE_URL!);
+export const db = drizzle({ schema, client: queryClient });
 
-export const db = drizzle(connection, { schema });
 export type DB = typeof db;
