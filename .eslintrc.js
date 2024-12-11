08dd68a482b2d79d2d13570781cc44ad540f27ed
Kiran <kiranojha1226@gmail.com>
2024-12-11 15:20:15 +0800

    chore: remove react compiler for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: "expo",
-  plugins: ["eslint-plugin-react-compiler"],
-  rules: {
-    "react-compiler/react-compiler": "error",
-  },
   ignorePatterns: ["/dist/*"],
 };
